3883a713239a4a73afac2c89eb6ba4b2a802ed21
Fernanda Borges <fernandaborges133@gmail.com>
2025-08-27 19:14:41 -0300

    new project

---
+++ SorteadorNumeros/sorteador.js
@@ -0,0 +1,14 @@
+function sortearNumero(){
+const qtd = parseInt(document.getElementById("numeros").value)
+const de = parseInt(document.getElementById("de").value)
+const ate = parseInt(document.getElementById("ate").value)
+const repeat = parseInt(document.getElementById("repeat").value)
+
+
+if(de >= ate){
+    console.log("numero inicial > que final")
+}
+
+
+}
+
